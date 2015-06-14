c09474dffbf9f5f22d17478bfc030f17862ffab9
Konrad <konri9@gmail.com>
2015-06-13 21:32:53 -0600

    varas

--- main.cpp
+++ main.cpp
@@ -15,11 +15,11 @@ using namespace std;
 //Construye una red con la cantidad de vértices y el promedio de
 //adyacencias por vértice correctos cuando cntVrt == 1000 y prbAdy == 0.5.
 
-template <typename V>
+
 void testConstructorGrafo()
 {
-    GrafoGnr< V > grafo (100,0.5);
-    GrafoGnr< V >  grafo2(1000,0.5);
+    GrafoGnr< int > grafo (100,0.5);
+    GrafoGnr< int >  grafo2(1000,0.5);
     if (grafo.obtTotVrt()!=100  || grafo2.obtTotVrt() != 1000)
     {
         std::cout << "%TEST_FAILED% FALLO EL CONSTRUCTOR DEL GRAFO(newsimpletest) message=ereror message sample" << std::endl;
@@ -29,13 +29,12 @@ void testConstructorGrafo()
 //Construye una copia idéntica a Grafo(100,0.5).
 //Construye una copia idéntica a Grafo(1000,0.5).
 
-template <typename V>
 void testConstructCopias()
 {
-    GrafoGnr< V >  orig(100,15);
-    GrafoGnr< V >  grafo(orig);
-    GrafoGnr< V >  orig2(1000,15);
-    GrafoGnr< V >  grafo2(orig2);
+    GrafoGnr< int >  orig(100,15);
+    GrafoGnr< int >  grafo(orig);
+    GrafoGnr< int >  orig2(1000,15);
+    GrafoGnr< int >  grafo2(orig2);
     if (!(grafo.obtTotVrt() == orig.obtTotVrt()) || !(grafo2.obtTotVrt() == orig2.obtTotVrt()))
     {
         std::cout << "%TEST_FAILED% FALLO EL CONSTRUCTOR DE COPIAS (newsimpletest) message=error message sample" << std::endl;
@@ -45,21 +44,19 @@ void testConstructCopias()
 //Construye el grafo correcto con el “redMuyPeq.txt”.
 //Construye el grafo correcto con el “redPeq.txt”.
 
-template <typename V>
 void testConstructorString()
 {
-    GrafoGnr< V >  grafo("redMuyPeq.txt");
-    GrafoGnr< V >  grafo2("redPeq.txt");
+    GrafoGnr< int >  grafo("redMuyPeq.txt");
+    GrafoGnr< int >  grafo2("redPeq.txt");
     if (grafo.obtTotVrt() != 10 || grafo2.obtTotVrt() != 100 ) //}|| grafo.obtAdy(0)[0] != 8 || grafo2.obtAdy(0)[0] != 2) {
     {
         std::cout << "%TEST_FAILED% FALLO EL CONSTRUCTOR A PARTIR DEL STRING (newsimpletest) message=error message sample" << std::endl;
     }
 }
 
-template <typename V>
-void testPromLongCmnsCrts(const V * )
+void testPromLongCmnsCrts()
 {
-    GrafoGnr< V >  grafo("redMuyPeq.txt");
+    GrafoGnr< int >  grafo("redMuyPeq.txt");
     double res = grafo.promLongCmnsCrts();
     if (!(0.4 > res > 0))
     {
@@ -68,10 +65,9 @@ void testPromLongCmnsCrts(const V * )
 }
 
 //Genera el valor correcto para el vértice indicado de un grafo muy pequeño.
-template <typename V>
 void testCoeficienteAgrupamiento()
 {
-    GrafoGnr< V >  grafo("redMuyPeq.txt");
+    GrafoGnr< int >  grafo("redMuyPeq.txt");
     double res = grafo.coeficienteAgrupamiento(0);
     if (!(res == 0))
     {
